ec97813cabb4d60d031454960ddd047dd8da4098
Sinay Mam <sinay.mam@pavilionpay.com>
2025-02-05 17:21:38 -0500

    Update home ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,24 +14,33 @@ export class HomePage {
   ngOnInit() {}
 
   generateSession() {
-    this.http.post(
-      'https://dev.api-gaming.paviliononline.io/sdk/api/patronsession/existing',
-      {
-        PatronId: '0ef56720-47b6-46bc-9a3a-b81bd511d10a',
-        VipCardNumber: '7210975245',
-        DateOfBirth: '04/15/1955',
-        remainingDailyDeposit: '1133.99',
-        walletBalance: '24',
-        transactionId: 'onfgh34isdngtest1',
-        transactionAmount: '1000',
-        transactionType: 'deposit',
-        productType: 0,
-        oneClickDiscountApplied: false,
-      }
-    ).subscribe({
-      next: (response) => console.log("Response from Session", response),
-      error: (e) => console.log("Error getting session")
-    });
+    this.http
+      .post(
+        'https://dev.api-gaming.paviliononline.io/sdk/api/patronsession/existing',
+        {
+          patronId: '1ef56720-47b6-46bc-9a3a-b11bd511d10b',
+          sessionId: '9a18a240-34eb-4223-ab41-7620e955b71d',
+          expires: '2025-02-05T16:46:30.7858665+00:00',
+          enrollmentStatus: 'complete',
+          vipCardNumber: '7210908875',
+          operatorName: 'Dev - Finicity Operator',
+          transactionSucceeded: false,
+          depositLimit: 1000,
+          minDeposit: 1,
+          withdrawLimit: 1000,
+          minWithdraw: 1,
+          transactionId: '19bca5f07d441938e0544a4d',
+          transactionAmount: 10,
+          returnUrl: 'closevip://done',
+          patronProductType: 'Preferred',
+          oneClickDiscountApplied: false,
+          oneTimePasscodePending: false,
+        }
+      )
+      .subscribe({
+        next: (response) => console.log('Response from Session', response),
+        error: (e) => console.log('Error getting session'),
+      });
   }
 
   launchVIPC() {
