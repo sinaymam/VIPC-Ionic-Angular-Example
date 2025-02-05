b6b3918dd3a8ba054661ae118b7e75e69ab1d327
Sinay Mam <sinay.mam@pavilionpay.com>
2025-02-05 15:26:38 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VIP-Connect-iOS-Ionic-Example',
+  webDir: 'www'
+};
+
+export default config;
