f878eabf70ecadd8567944d575f3cfcd4a1090a1
Sinay Mam <sinay.mam@pavilionpay.com>
2025-02-06 17:34:18 -0500

    Launching SDK with sessionId - will need to add it in env file

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Renderer2, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -9,47 +9,9 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/a
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton ],
 })
 export class HomePage {
-  constructor(private renderer: Renderer2, private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit() {}
-
-  generateSession() {
-    this.http
-      .post(
-        'https://dev.api-gaming.paviliononline.io/sdk/api/patronsession/existing',
-        {
-          patronId: '1ef56720-47b6-46bc-9a3a-b11bd511d10b',
-          sessionId: '9a18a240-34eb-4223-ab41-7620e955b71d',
-          expires: '2025-02-05T16:46:30.7858665+00:00',
-          enrollmentStatus: 'complete',
-          vipCardNumber: '7210908875',
-          operatorName: 'Dev - Finicity Operator',
-          transactionSucceeded: false,
-          depositLimit: 1000,
-          minDeposit: 1,
-          withdrawLimit: 1000,
-          minWithdraw: 1,
-          transactionId: '19bca5f07d441938e0544a4d',
-          transactionAmount: 10,
-          returnUrl: 'closevip://done',
-          patronProductType: 'Preferred',
-          oneClickDiscountApplied: false,
-          oneTimePasscodePending: false,
-        }
-      )
-      .subscribe({
-        next: (response) => console.log('Response from Session', response),
-        error: (e) => console.log('Error getting session'),
-      });
-  }
-
-  launchVIPC() {
-    this.generateSession();
-    const script = this.renderer.createElement('script');
-    script.src =
-      'https://stsdkkazusenadev01.blob.core.windows.net/frontend/vip-preferred.min.js';
-    script.type = 'type/javascript';
-    script.async = true;
-    document.body.appendChild(script);
+  async launchVIPC() {
+    window.location.href = `${environment.vipc}/sdk?mode=deposit#${environment.devTestOperator.sessionId}`;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  vipc: 'https://dev.api-gaming.paviliononline.io',
+  devTestOperator: {
+    sessionId: '' // Use session-id here
+  }
 };
 
 /*
